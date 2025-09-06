83587dafe1ff325f3b67758e25dc204556da3580
rtasouji <r.tasouji@gmail.com>
2025-09-07 00:29:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -31,8 +31,8 @@ def build_urls(job_title: str, location: str) -> dict:
     loc_dash = hyphenate(location)
 
     return {
-    # "Adzuna":  f"https://www.adzuna.co.uk/jobs/search?q={job_title}&w={location}",
-    # "CWJobs":  f"https://www.cwjobs.co.uk/jobs/{job_dash}/in-{loc_dash}?radius=10&searchOrigin=Resultlist_top-search",
+     "Adzuna":  f"https://www.adzuna.co.uk/jobs/search?q={job_title}&w={location}",
+     "CWJobs":  f"https://www.cwjobs.co.uk/jobs/{job_dash}/in-{loc_dash}?radius=10&searchOrigin=Resultlist_top-search",
     # "TotalJobs": f"https://www.totaljobs.com/jobs/{job_dash}/in-{loc_dash}?radius=10&searchOrigin=Resultlist_top-search",
           "Indeed":  f"https://uk.indeed.com/jobs?q={job_title}&l={location}",
     # "Reed":  f"https://www.reed.co.uk/jobs/{job_dash}-jobs-in-{loc_dash}",
